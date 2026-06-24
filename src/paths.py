7f859a8ffb11debe94fd4bb799aae89b83b8ac95
Ahmed Sherif <as4349124@gmail.com>
2026-06-24 11:56:17 +0300

    Builds model training pipeline

--- src/paths.py
+++ src/paths.py
@@ -5,7 +5,9 @@ PARENT_DIR = Path(__file__).parent.resolve().parent
 DATA_DIR = PARENT_DIR / 'data'
 RAW_DATA_DIR = PARENT_DIR / 'data' / 'raw'
 TRASNFORMED_DATA_DIR = PARENT_DIR / 'data' / 'transformed'
- 
+
+MODELS_DIR = PARENT_DIR / 'models'
+
 if not Path(DATA_DIR).exists():
     os.mkdir(DATA_DIR)
 
@@ -14,3 +16,6 @@ if not Path(RAW_DATA_DIR).exists():
 
 if not Path(TRASNFORMED_DATA_DIR).exists():
     os.mkdir(TRASNFORMED_DATA_DIR)
+
+if not Path(MODELS_DIR).exists():
+    os.mkdir(MODELS_DIR)
